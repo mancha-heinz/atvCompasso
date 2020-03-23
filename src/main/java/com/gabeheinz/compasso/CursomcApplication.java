855065d5b5a3ddc5b01e4dbc54390703c164580a
Gabriel Heinz <manchaheinz@gmail.com>
2020-03-23 10:14:14 -0300

    Realizando Alteracoes

--- src/main/java/com/gabeheinz/compasso/CursomcApplication.java
+++ src/main/java/com/gabeheinz/compasso/CursomcApplication.java
@@ -1,4 +1,4 @@
-package com.nelioalves.cursomc;
+package com.gabeheinz.compasso;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/gabeheinz/compasso/CursomcApplicationTests.java
+++ src/test/java/com/gabeheinz/compasso/CursomcApplicationTests.java
@@ -1,4 +1,4 @@
-package com.nelioalves.cursomc;
+package com.gabeheinz.compasso;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
